8496dbe706f19efb7fcb3085590aff3fa917a0a0
Gabriel Packer <ggkpacker@gmail.com>
2019-04-30 20:56:56 -0300

    Add useless user test to verify rspec config

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe User do
-  it_is_expected_to have_attributes(:email, :password)
+  subject(:subject) { described_class.new(email: 'foo.bar@email.com', password: '123123') }
+
+  it { is_expected.to have_attributes(email: 'foo.bar@email.com') }
 end
